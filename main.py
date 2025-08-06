552416ac047402d6d36e70d0a592e7cac238f309
Andrei Cret <alexandru.cret04@gmail.com>
2025-08-06 12:33:54 +0300

    Added tests

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ tree.add(0)
 tree.add(8)
 tree.add(2)
 tree.printTree()
+
+
+def test_find_existing_node():
+    tree = Tree()
+    for val in [5, 3, 7, 1, 4]:
+        tree.add(val)
+    node = tree.find(3)
+    assert node is not None
+    assert node.data == 3
+
+def test_find_nonexistent_node():
+    tree = Tree()
+    for val in [5, 3, 7]:
+        tree.add(val)
+    node = tree.find(10)
+    assert node is None
